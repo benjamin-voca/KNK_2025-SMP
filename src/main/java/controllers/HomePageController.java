c9a5037fa674318dee4c551d06d1264e3c16dcad
Erblin Syla <erblin.sylaa@gmail.com>
2025-05-05 20:52:06 +0200

    Shtimi i konfirmimit para Log Out

--- src/main/java/controllers/HomePageController.java
+++ src/main/java/controllers/HomePageController.java
@@ -1,4 +1,28 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomePageController {
+    @FXML
+    private void goToLogOut(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/logout.fxml"));
+        Parent root = loader.load();
+
+        Stage logOutStage = new Stage();
+        logOutStage.setScene(new Scene(root));
+        logOutStage.setTitle("Log Out");
+        logOutStage.show();
+
+        LogOutController controller = loader.getController();
+        Stage homeStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        controller.setPreviousStages(homeStage, logOutStage);
+    }
 }
---
+++ src/main/java/controllers/LogOutController.java
@@ -0,0 +1,46 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LogOutController {
+    private Stage homeStage;
+    private Stage logOutStage;
+
+    public void setPreviousStages(Stage homePage, Stage logOutPage) {
+        this.homeStage = homePage;
+        this.logOutStage = logOutPage;
+    }
+
+    @FXML
+    private void goToIndex(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/index.fxml"));
+        Parent root = loader.load();
+
+        Stage indexStage = new Stage();
+        indexStage.setScene(new Scene(root));
+        indexStage.setTitle("Welcome");
+        indexStage.show();
+
+        if (homeStage != null) {
+            homeStage.close();
+        }
+
+        if (logOutStage != null) {
+            logOutStage.close();
+        }
+    }
+
+    @FXML
+    private void closeLogOut(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+}
