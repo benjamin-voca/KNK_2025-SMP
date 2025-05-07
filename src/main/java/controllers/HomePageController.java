0e6019a18841414b864e3a0ca9f12ea923d58afe
Erblin Syla <erblin.sylaa@gmail.com>
2025-05-07 15:44:29 +0200

    Shtimi i butonit per te shfaqur dhe larguar Sidebar

--- src/main/java/controllers/HomePageController.java
+++ src/main/java/controllers/HomePageController.java
@@ -1,16 +1,50 @@
 package controllers;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class HomePageController {
+
+    @FXML
+    private AnchorPane sideBar;
+
+    @FXML
+    private Button toggleButton;
+
+    private boolean sidebarVisible = true;
+
+    @FXML
+    private void toggleSideBar() {
+        double sidebarWidth = sideBar.getWidth();
+
+        TranslateTransition slideTransition = new TranslateTransition(Duration.millis(300), sideBar);
+        TranslateTransition buttonTransition = new TranslateTransition(Duration.millis(300), toggleButton);
+
+        if (sidebarVisible) {
+            slideTransition.setByX(-sidebarWidth);
+            buttonTransition.setByX(-sidebarWidth);
+        } else {
+            slideTransition.setByX(sidebarWidth);
+            buttonTransition.setByX(sidebarWidth);
+        }
+
+        slideTransition.play();
+        buttonTransition.play();
+
+        sidebarVisible = !sidebarVisible;
+    }
+
     @FXML
     private void goToLogOut(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/logout.fxml"));
