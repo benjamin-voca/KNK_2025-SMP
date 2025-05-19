2e03ec5bdb907b1efee197d3fc3da91c4ae3f29c
DiellartMulolli <diellart.mulolli@student.uni-pr.edu>
2025-05-19 12:31:05 +0200

    Register page fxml complete

--- src/main/java/pages/Registerpage.java
+++ src/main/java/pages/Registerpage.java
@@ -3,12 +3,13 @@ package pages;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Registerpage extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        System.out.println(getClass().getResource("/utilities/Up_frame.png"));
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/registerpage.fxml"));
         Scene scene = new Scene(loader.load(), 800, 700);
         primaryStage.setTitle("Registration Page");
