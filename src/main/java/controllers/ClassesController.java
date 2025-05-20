8f8b4e902176dde66a3039fcac4e200db636f975
bashkimhashani <bashkim.hashani@student.uni-pr.edu>
2025-05-20 13:21:23 +0200

    update transfer page

--- src/main/java/controllers/ClassesController.java
+++ src/main/java/controllers/ClassesController.java
@@ -161,6 +161,18 @@ public class ClassesController {
     }
 
     @FXML
+    private void goToTransfer(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/transfer.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Transfer Request");
+        stage.show();
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+    }
+
+    @FXML
     private void goToLogOut(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/logout.fxml"));
         Parent root = loader.load();
